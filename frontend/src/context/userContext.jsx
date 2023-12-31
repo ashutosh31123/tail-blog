877f6d0c3a80a216f03edb736fe7d4b58bb539c2
Ashutosh Panda <b421012@iiit-bh.ac.in>
2023-12-31 12:17:04 +0530

    5th Draft

--- frontend/src/context/userContext.jsx
+++ frontend/src/context/userContext.jsx
@@ -1,28 +1,34 @@
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
+import { URL } from "../url";
+
 
 // eslint-disable-next-line react-refresh/only-export-components
-export const userContext= createContext({})
+export const userContext = createContext({})
 
 // eslint-disable-next-line react/prop-types
-export function UserContextProvider({children}){
-    const [user,setUser]=useState(null)
+export function UserContextProvider({ children }) {
+  const [user, setUser] = useState(null)
 
-    useEffect(()=>{
-        getUser()
-    },[])
+  useEffect(() => {
+    getUser()
+  }, [])
 
-    const getUser=async()=>{
-        try{
-            const res=await axios.get(URL+"/api/auth/refetch",{withCredentials:true})
-            console.log(res.data)
-        }
-        catch(err){
-         console.log(err)    
-        }
+  const getUser=async()=>{
+    try{
+      const res=await axios.get(URL+"/api/auth/refetch",{withCredentials:true})
+      // console.log(res.data)
+      setUser(res.data)
+
     }
+    catch(err){
+      console.log(err)
+    }
+  }
 
-    return (<userContext.Provider value={{user,setUser}}>
+  return (
+    <userContext.Provider value={{ user, setUser }}>
       {children}
-    </userContext.Provider>)
+    </userContext.Provider>
+  );
 }
